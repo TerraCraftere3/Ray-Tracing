380c5cf91ff5773c2d257864ba256cf9b815dae2
TerraCrafterE3 <terracraftere3@gmail.com>
2025-03-02 09:19:27 +0100

    Added spdlog logging

---
+++ Ray-Tracing/src/Core.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+#ifdef Windows
+	#define PLATFORM_STRING "Windows"
+	#define PLATFORM Windows
+#else
+	#error Other Architectures are not supported yet
+#endif
+
+
+void Empty() { }
+
+template<typename First, typename ... Object>
+void Empty(First arg, const Object&... rest) { 
+	Empty(rest...);
+}
+
+
+// LOGGER
+
+#ifdef DEBUG
+#define LOG_INFO spdlog::get("console")->info
+#define LOG_DEBUG spdlog::get("console")->debug
+#define LOG_WARN spdlog::get("console")->warn
+#define LOG_ERROR spdlog::get("console")->error
+#elif RELEASE
+#define LOG_INFO Empty
+#define LOG_DEBUG Empty
+#define LOG_WARN Empty
+#define LOG_ERROR spdlog::get("console")->error
+#endif
+
+static void initLogger() {
+	auto console = spdlog::stdout_color_mt("console");
+	spdlog::set_pattern("[%H:%M:%S] [thread %t] %^%v%$");
+	spdlog::set_level(spdlog::level::debug);
+	LOG_INFO("Logger initialized!");
+	LOG_DEBUG("Debug Test");
+	LOG_WARN("Warn Test, {}", 123.456f);
+	LOG_ERROR("Error Test, {}:{}", "File not found", 404);
+	system("PAUSE");
+}
